207063aa53a411eca8b50b19fd35e22d71c5b363
p@localhost <p@localhost>
2022-06-13 16:48:12 -0500

    Improves key maps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,11 +5,12 @@ local map = vim.api.nvim_set_keymap
 
 options = { noremap = true }
 map('n', '<F2>', "<cmd>Lspsaga rename<cr>", options)
+map('n', '<F3>', "<cmd>Lspsaga lsp_finder<cr>", options)
 map('n', 'gx',   "<cmd>Lspsaga code_action<cr>", options)
 map('x', 'gx',   ":<c-u>Lspsaga range_code_action<cr>", options)
-map('n', '<F3>', "<cmd>Lspsaga hover_doc<cr>", options)
+map('n', 'gi', "<cmd>Lspsaga hover_doc<cr>", options)
 map('n', 'go',   "<cmd>Lspsaga show_line_diagnostics<cr>", options)
 map('n', 'gj',   "<cmd>Lspsaga diagnostic_jump_next<cr>", options)
 map('n', 'gk',   "<cmd>Lspsaga diagnostic_jump_prev<cr>", options)
-map('n', '<C-u>',"<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1, '<c-u>')<cr>", {})
-map('n', '<C-d>',"<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1, '<c-d>')<cr>", {})
+-- map('n', '<C-u>',"<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1, '<c-u>')<cr>", {})
+-- map('n', '<C-d>',"<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1, '<c-d>')<cr>", {})

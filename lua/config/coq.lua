92b382944d742c2eb61ac44bb0df00f192b49eb1
p@localhost <p@localhost>
2023-05-04 16:01:20 -0600

    fixes null vimscript coq

--- lua/config/coq.lua
+++ lua/config/coq.lua
@@ -1,4 +1,4 @@
 -- vim.g.coq_settings.keymap.jump_to_mark = '<C-;>'
 vim.cmd [[
-let g:coq_settings = { 'auto_start' : 'shut-up', 'keymap.jump_to_mark' : '<C-;>', 'keymap.bigger_preview' : 'null', 'keymap.manual_complete' : 'null' }
+let g:coq_settings = { 'keymap.jump_to_mark' : '<C-;>', 'keymap.bigger_preview' : v:null, 'keymap.manual_complete' : v:null, 'auto_start' : 'shut-up' }
 ]]

b82e97812e112ab485eb26c8849caad190be1564
p@localhost <p@localhost>
2023-05-04 14:34:19 -0600

    telescope keybinds

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,7 +1,17 @@
 -- require('telescope').load_extension('smart_history')
+local actions = require("telescope.actions")
+
 require('telescope').setup {
   defaults = {
     wrap_results = true,
+
+		mappings = {
+			i = {
+				["<C-k>"] = actions.move_selection_previous, -- move to prev result
+				["<C-j>"] = actions.move_selection_next, -- move to next result
+			},
+		},
+
     history = {
       path = '~/.local/share/nvim/databases/telescope_history.sqlite3',
       limit = 100

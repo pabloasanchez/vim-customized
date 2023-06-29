bf2f5e7a7235f48d9676f2e85ece637570f7b1e2
p@localhost <p@localhost>
2023-06-28 22:08:38 -0600

    removes autocomplete for cmd mode and search

--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -67,21 +67,16 @@ cmp.setup.filetype('gitcommit', {
 })
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
-  }
-})
+-- cmp.setup.cmdline({ '/', '?' }, {
+--   mapping = cmp.mapping.preset.cmdline(),
+--   sources = {{ name = 'buffer' }}
+-- })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-      { name = 'cmdline' }
-    })
-})
+-- cmp.setup.cmdline(':', {
+--   mapping = cmp.mapping.preset.cmdline(),
+--   -- sources = cmp.config.sources({{ name = 'path' }}, {{ name = 'cmdline' }}) 
+--   sources = cmp.config.sources({{ name = 'path' }}, {{ name = 'cmdline' }}) 
+-- })
 
 

7d99aafaa18edaccc10d39dd2a6dd5db614852a2
p@localhost <p@localhost>
2023-12-20 00:53:41 -0600

    lsp toggle/win

--- lua/commands/lsp.lua
+++ lua/commands/lsp.lua
@@ -24,6 +24,16 @@ function lspDiagPrev()
   fn.execute(':LspUI diagnostic prev')
 end
 
+function lspDiagToggle()
+  local config = vim.diagnostic.config
+  local vt = config().virtual_text
+  config({ virtual_text = not vt, underline = not vt, signs = not vt })
+end
+
+function lspDiagWindow()
+  fn.execute(':TroubleToggle')
+end
+
 function lspPeek()
   fn.execute(':LspUI peek_definition')
 end
@@ -33,32 +43,32 @@ function lspReferences()
 end
 
 function lspOutline()
-  fn.execute(':Navbuddy')
+  fn.execute(':silent! Navbuddy')
 end
 
 function lspFormat()
-  vim.lsp.buf.format() 
+  vim.lsp.buf.format()
 end
 
 function lspImplementation()
-  vim.lsp.buf.implementation() 
+  vim.lsp.buf.implementation()
 end
 
 function lspLineDiagnostics()
-  vim.diagnostic.open_float(nil, {focus=false})
+  vim.diagnostic.open_float(nil, { focus = false })
 end
 
 function lspSymbols()
-  fn.execute(':Telescope lsp_document_symbols') 
+  fn.execute(':Telescope lsp_document_symbols')
 end
 
 function lspTypeDefs()
-  fn.execute(':Glance type_definitions') 
+  fn.execute(':Glance type_definitions')
 end
 
 return {
-  lspHover, lspRename, lspCodeAction, lspDefinitions, 
-  lspDiagNext, lspDiagPrev, lspPeek, lspReferences, 
-  lspOutline, lspFormat, lspImplementation, lspLineDiagnostics, 
-  lspSymbols, lspTypeDefs 
+  lspHover, lspRename, lspCodeAction, lspDefinitions,
+  lspDiagWindow, lspDiagToggle, lspDiagNext, lspDiagPrev, lspPeek, lspReferences,
+  lspOutline, lspFormat, lspImplementation, lspLineDiagnostics,
+  lspSymbols, lspTypeDefs
 }

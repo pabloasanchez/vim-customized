80e3193f0b165f7744e87037c5dd782de8c02ace
p@localhost <p@localhost>
2023-12-31 02:45:37 -0600

    comments out zen related autocmds

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -26,23 +26,25 @@ api.nvim_create_autocmd( "InsertLeave", {
   command = "UserHighlightsNormal"
 })
 
-api.nvim_create_autocmd( { "BufNew" }, { 
- pattern = "*",
- command = "lua if #vim.api.nvim_tabpage_list_wins(0) == 1 and vim.bo.ft ~= 'alpha' then vim.cmd('Zen') end"
-})
-
-api.nvim_create_autocmd( { "BufWinEnter", "VimEnter", "BufReadPost" }, { 
- pattern = "*",
-  command = "lua if vim.bo.ft == 'no-neck-pain' then vim.cmd[[wincmd l]] end"
-})
-
 api.nvim_create_autocmd({ "WinNew", "ColorScheme" }, {
   pattern = "*",
   command = "UserHighlights",
 })
 
--- api.nvim_create_autocmd({ "BufRead", "VimEnter" }, {
---   pattern = "*",
---   command = "let &l:modifiable = !&readonly",
+-- Zen mode (NNP) Autocommands to attempt to automatically enter Zen mode
+
+-- api.nvim_create_autocmd( { "BufNew" }, { 
+--  pattern = "*",
+--  command = "lua if #vim.api.nvim_tabpage_list_wins(0) == 1 and vim.bo.ft ~= 'alpha' then vim.cmd('Zen') end"
 -- })
+--
+-- api.nvim_create_autocmd( { "BufWinEnter", "VimEnter", "BufReadPost" }, { 
+--  pattern = "*",
+--   command = "lua if vim.bo.ft == 'no-neck-pain' then vim.cmd[[wincmd l]] end"
+-- })
+
+-- -- api.nvim_create_autocmd({ "BufRead", "VimEnter" }, {
+-- --   pattern = "*",
+-- --  command = "let &l:modifiable = !&readonly",
+-- -- })
 

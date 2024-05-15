29e1c1e2ebfdb7ec03b226da05b81415776d24ba
p@localhost <p@localhost>
2024-05-15 02:49:18 -0600

    setting select to false to only confirm explicitly selected items

--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -20,7 +20,7 @@ cmp.setup({
   },
   mapping = {
     -- ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only c irm explicitly selected items.
+    ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only c irm explicitly selected items.
     -- ['<Esc>'] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Insert, select = false },
     -- ['<Esc>'] = cmp.mapping.close() -- cmp.mapping.confirm({ select = false }),
     ['<Esc>'] = cmp.mapping(function(fallback)

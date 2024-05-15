258ee2cdb8f22af049a7ef4ecba91c42d7d1e59b
p@localhost <p@localhost>
2024-05-15 02:48:22 -0600

    commenting out telescope bookmarks plugin

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -27,5 +27,6 @@ require('telescope').setup {
 -- require('telescope').load_extension('smart_history')
 require('telescope').load_extension('glyph')
 require('telescope').load_extension('ag')
+-- require('telescope').load_extension('bookmarks')
 
 require'telescope-all-recent'.setup({})

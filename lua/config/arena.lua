ac4cd94cfe124f59edfc9f711bc5ca1210304e31
p@localhost <p@localhost>
2024-05-15 02:47:06 -0600

    adds arena + plugins.lua formatting

---
+++ lua/config/arena.lua
@@ -0,0 +1,45 @@
+require('arena').setup(
+{
+  -- Maxiumum number of files that the arena window can contain, or `nil` for
+  -- an unlimited amount
+  max_items = 100,
+  -- Always show the enclosing folder for these paths
+  always_context = { "mod.rs", "init.lua" },
+  -- When set, ignores the current buffer when listing files in the window.
+  ignore_current = true,
+  -- Options to apply to the arena buffer.
+  -- Format should be `["<OPTION>"] = <VALUE>`
+  buf_opts = {
+    -- Example. Uncomment to add to your config!
+    -- ["relativenumber"] = false,
+  },
+  -- Filter out buffers per the project they belong to.
+  per_project = false,
+
+
+  window = {
+    width = 50,
+    height = 40,
+    border = "rounded",
+
+    -- Options to apply to the arena window.
+    opts = {},
+  },
+
+  -- Keybinds for the arena window.
+  keybinds = {
+      -- Example. Uncomment to add to your config!
+      -- ["e"] = function()
+      --   vim.cmd("echo \"Hello from the arena!\"")
+      -- end
+  },
+
+  -- Config for frecency algorithm.
+  algorithm = {
+    -- Multiplies the recency by a factor. Must be greater than zero.
+    -- A smaller number will mean less of an emphasis on recency!
+    recency_factor = 0.9,
+    -- Same as `recency_factor`, but for frequency!
+    frequency_factor = 0.5,
+  },
+})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -9,49 +9,52 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin', PLUGINS)
 
 --  Initial options and defaults
-Plug 'airblade/vim-rooter'          -- vim-rooter - changes working dir to the project root
+Plug 'airblade/vim-rooter' -- vim-rooter - changes working dir to the project root
 
 -- File management
-Plug 'stevearc/oil.nvim'            -- File mgmt using a buffer
+Plug 'stevearc/oil.nvim' -- File mgmt using a buffer
 
 --  Window splits
-Plug 'simeji/winresizer'            -- Resize splits with hjkl, using :WinResizerStartResize
-Plug 'qpkorr/vim-bufkill'           -- Closes buffers
-Plug 'vimlab/split-term.vim'        -- Better Terminal splits
+Plug 'simeji/winresizer'     -- Resize splits with hjkl, using :WinResizerStartResize
+Plug 'qpkorr/vim-bufkill'    -- Closes buffers
+Plug 'vimlab/split-term.vim' -- Better Terminal splits
+
+-- Buffers
+Plug 'dzfrias/arena.nvim'
 
 --  GUI
-Plug 'pabloasanchez/vim-quickui'      -- Top Menu
+Plug 'pabloasanchez/vim-quickui' -- Top Menu
 Plug 'jinzhongjia/LspUI.nvim'
-Plug 'SmiteshP/nvim-navic'          -- navbuddy dependency (crumbs)
-Plug 'MunifTanjim/nui.nvim'         -- navbuddy dependency (ui)
-Plug 'SmiteshP/nvim-navbuddy'       -- navbuddy - LSP Outline in a floating window
-Plug 'folke/trouble.nvim'           -- Error messages / diagnostics
+Plug 'SmiteshP/nvim-navic'       -- navbuddy dependency (crumbs)
+Plug 'MunifTanjim/nui.nvim'      -- navbuddy dependency (ui)
+Plug 'SmiteshP/nvim-navbuddy'    -- navbuddy - LSP Outline in a floating window
+Plug 'folke/trouble.nvim'        -- Error messages / diagnostics
 
 --  Language-specific
 Plug 'eliba2/vim-node-inspect'
 
 --  Search
-Plug ('junegunn/fzf', {dir = '~/.fzf', ['do'] = './install --all' })        --  fzf - Multi use Fuzzy Finder
-Plug 'junegunn/fzf.vim'            --  fzf - Multi use Fuzzy Finder
-Plug 'rhysd/clever-f.vim'          --  clever-f - clever-f.vim extends f, F, t and T mappings for more convenience 
-Plug 'nvim-lua/plenary.nvim'       --  Required by Telescope and spectre
-Plug 'nvim-pack/nvim-spectre'      --  A search panel for neovim.
-Plug 'rlane/pounce.nvim'           --  Fuzzy jump search
+Plug('junegunn/fzf', { dir = '~/.fzf', ['do'] = './install --all' }) --  fzf - Multi use Fuzzy Finder
+Plug 'junegunn/fzf.vim'                                              --  fzf - Multi use Fuzzy Finder
+Plug 'rhysd/clever-f.vim'                                            --  clever-f - clever-f.vim extends f, F, t and T mappings for more convenience
+Plug 'nvim-lua/plenary.nvim'                                         --  Required by Telescope and spectre
+Plug 'nvim-pack/nvim-spectre'                                        --  A search panel for neovim.
+Plug 'rlane/pounce.nvim'                                             --  Fuzzy jump search
 
 --  Telescope
 Plug 'nvim-telescope/telescope.nvim'
 Plug 'ghassan0/telescope-glyph.nvim'
 Plug 'kelly-lin/telescope-ag'
-Plug 'kkharji/sqlite.lua'         -- Required by telescope-all-recent
+Plug 'kkharji/sqlite.lua' -- Required by telescope-all-recent
 Plug 'prochri/telescope-all-recent.nvim'
 
 --  Edit tools
 -- Plug 't9md/vim-textmanip'             --  t9md/vim-textmanip - Move/duplicate text intuitively
 Plug 'booperlv/nvim-gomove'
-Plug 'tpope/vim-surround'             --  Surroundings yss, ysiw, cs' ds', S
-Plug 'tpope/vim-repeat'               --  repeat.vim - remaps . in a way that plugins can tap into it
-Plug 'tpope/vim-commentary'           --  commentary.vim - comment stuff out
-Plug 'alvan/vim-closetag'             --  Automatically closes x/html tags
+Plug 'tpope/vim-surround'   --  Surroundings yss, ysiw, cs' ds', S
+Plug 'tpope/vim-repeat'     --  repeat.vim - remaps . in a way that plugins can tap into it
+Plug 'tpope/vim-commentary' --  commentary.vim - comment stuff out
+Plug 'alvan/vim-closetag'   --  Automatically closes x/html tags
 Plug 'numToStr/Comment.nvim'
 Plug 'tenxsoydev/karen-yank.nvim'
 
@@ -59,11 +62,11 @@ Plug 'tenxsoydev/karen-yank.nvim'
 Plug 'AckslD/nvim-neoclip.lua'
 
 --  Gutter
-Plug 'kshenoy/vim-signature'     --  vim-signature - Place, toggle, display marks
+Plug 'kshenoy/vim-signature' --  vim-signature - Place, toggle, display marks
 -- Plug 'lewis6991/gitsigns.nvim'    --  Super fast git decorations implemented purely in lua/teal.
 
 --  Terminal
-Plug 'voldikss/vim-floaterm'      --  floaterm - Floating Terminal for vim
+Plug 'voldikss/vim-floaterm' --  floaterm - Floating Terminal for vim
 
 --  Start screen
 Plug 'goolord/alpha-nvim'
@@ -80,7 +83,7 @@ Plug 'dnlhc/glance.nvim'
 
 -- Snips
 Plug 'rafamadriz/friendly-snippets'
-Plug ('L3MON4D3/LuaSnip', { ['tag'] = 'v2.1.1', ['do'] = 'make install_jsregexp' })
+Plug('L3MON4D3/LuaSnip', { ['tag'] = 'v2.1.1', ['do'] = 'make install_jsregexp' })
 
 -- Completion
 -- Sources
@@ -88,15 +91,15 @@ Plug 'hrsh7th/cmp-nvim-lsp'
 Plug 'hrsh7th/cmp-buffer'
 -- Plug 'hrsh7th/cmp-path'
 -- Plug 'hrsh7th/cmp-cmdline'
-Plug 'hrsh7th/nvim-cmp'            -- Main plugin
+Plug 'hrsh7th/nvim-cmp' -- Main plugin
 
 Plug 'saadparwaiz1/cmp_luasnip'
 
 -- TS Manager
-Plug ('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' })
+Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' })
 
 --  Distraction free
-Plug ('pabloasanchez/yazmp')
+Plug('pabloasanchez/yazmp')
 -- Plug ('shortcuts/no-neck-pain.nvim', { ['tag'] = '*' })
 
 vim.call('plug#end')
@@ -117,10 +120,9 @@ require('config/lspconfig')
 require('config/lspui')
 require('config/gomove')
 require('config/trouble')
+require('config/arena')
 
 -- Optionless -  require('my_plugin').setup()
 require('Comment').setup()
 require('spectre').setup()
--- require('no-neck-pain').setup({ disableOnLastBuffer = false, width = 130 })
-require('luasnip.loaders.from_vscode').lazy_load()     --   Luasnip: Activates friendly-snippets
-
+require('luasnip.loaders.from_vscode').lazy_load() --   Luasnip: Activates friendly-snippets

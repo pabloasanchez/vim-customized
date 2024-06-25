f509bf851f9a00505a06da695160c90b427809b3
p@localhost <p@localhost>
2024-06-25 06:14:35 -0600

    alpha colors

--- lua/config/alpha-nvim.lua
+++ lua/config/alpha-nvim.lua
@@ -1,6 +1,13 @@
 local alpha = require('alpha')
 local dashboard = require('alpha.themes.dashboard')
 
+local function button(sc, txt, keybind, keybind_opts)
+  local b = dashboard.button(sc, txt, keybind, keybind_opts)
+  b.opts.hl = "Delimiter"
+  b.opts.hl_shortcut = "Identifier"
+  return b
+end
+
 -- Set header
 dashboard.section.header.val = {
   "                             ⠀⠀⠀⠀",
@@ -20,15 +27,16 @@ dashboard.section.header.val = {
   "                             ⠀⠀⠀⠀",
   "                             ⠀⠀⠀⠀",
 }
-
+dashboard.section.header.opts.hl = 'Identifier'
 -- Set menu
 dashboard.section.buttons.val = {
-  dashboard.button("e", "  > New file", ":enew <BAR> Zen <ENTER>"),
-  -- dashboard.button("f", "  > Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
-  dashboard.button("r", "  > Recent", ":Recent <ENTER>"),
-  dashboard.button("s", "  > Settings", ":e ~/.vim"),
-  dashboard.button("q", "  > Quit NVIM", ":qa <ENTER>"),
+  button("e", "  New file", ":enew <BAR> Zen <ENTER>"),
+  -- button("f", "  Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
+  button("r", "  Recent", ":Recent <ENTER>"),
+  button("s", "  Settings", ":e ~/.vim"),
+  button("q", "  Quit NVIM", ":qa <ENTER>"),
 }
+
 
 -- Send config to alpha
 alpha.setup(dashboard.opts)

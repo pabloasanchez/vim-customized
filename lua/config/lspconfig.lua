f8a40dfb4318e859d886e9c723e745153a7a7c3f
p@localhost <p@localhost>
2026-04-01 04:00:57 -0600

    lspconfig deprecation update

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -1,6 +1,4 @@
 -- Setup language servers.
-local configs = require('lspconfig/configs')
-local lspconfig = require('lspconfig')
 local completion = require('blink.cmp')
 
 --Enable (broadcasting) snippet capability for completion
@@ -9,17 +7,17 @@ local completion = require('blink.cmp')
 -- local capabilities = completion.default_capabilities()  -- nvim-cmp
 local capabilities = completion.get_lsp_capabilities()     -- blink
 
-lspconfig.clangd.setup { on_attach = on_attach, capabilities = capabilities }
-lspconfig.cssls.setup { on_attach = on_attach, capabilities = capabilities }
-lspconfig.tailwindcss.setup { on_attach = on_attach, capabilities = capabilities }
-lspconfig.emmet_language_server.setup { on_attach = on_attach, capabilities = capabilities }
-lspconfig.emmet_ls.setup { on_attach = on_attach, capabilities = capabilities }
-lspconfig.pyright.setup { on_attach = on_attach, capabilities = capabilities }
-lspconfig.ts_ls.setup { on_attach = on_attach, capabilities = capabilities }
-lspconfig.lua_ls.setup { on_attach = on_attach, capabilities = capabilities }
-lspconfig.rust_analyzer.setup { on_attach = on_attach, capabilities = capabilities }
+vim.lsp.config('clangd', { on_attach = on_attach, capabilities = capabilities })
+vim.lsp.config('cssls', { on_attach = on_attach, capabilities = capabilities })
+vim.lsp.config('tailwindcss', { on_attach = on_attach, capabilities = capabilities })
+vim.lsp.config('emmet_language_server', { on_attach = on_attach, capabilities = capabilities })
+vim.lsp.config('emmet_ls', { on_attach = on_attach, capabilities = capabilities })
+vim.lsp.config('pyright', { on_attach = on_attach, capabilities = capabilities })
+vim.lsp.config('ts_ls', { on_attach = on_attach, capabilities = capabilities })
+vim.lsp.config('lua_ls', { on_attach = on_attach, capabilities = capabilities })
+vim.lsp.config('rust_analyzer', { on_attach = on_attach, capabilities = capabilities })
 
-lspconfig.html.setup {
+vim.lsp.config('html', {
   on_attach = on_attach,
   capabilities = capabilities,
   init_options = {
@@ -30,7 +28,18 @@ lspconfig.html.setup {
     },
     provideFormatter = true
   }
-}
+})
+
+vim.lsp.enable('clangd')
+vim.lsp.enable('cssls')
+vim.lsp.enable('tailwindcss')
+vim.lsp.enable('emmet_language_server')
+vim.lsp.enable('emmet_ls')
+vim.lsp.enable('pyright')
+vim.lsp.enable('ts_ls')
+vim.lsp.enable('lua_ls')
+vim.lsp.enable('rust_analyzer')
+vim.lsp.enable('html')
 
 -- Other LSP configs
 vim.diagnostic.config({ virtual_text = false, underline = true, signs = false, })
